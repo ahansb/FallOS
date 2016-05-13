1fdfac3b80cc5af5a68066c4e35a962c2adc95d1
ahansb <andrejboyadjiev@gmail.com>
2016-05-13 16:22:07 +0300

    Initial commit - may be used as template for web api

---
+++ FallOS/Data/FallOs.Data/FallOSDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace FallOs.Data
+{
+    using FallOS.Data.Models;
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    public class FallOSDbContext : IdentityDbContext<User>
+    {
+        public FallOSDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static FallOSDbContext Create()
+        {
+            return new FallOSDbContext();
+        }
+    }
+}
